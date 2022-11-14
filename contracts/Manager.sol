94d9a89cca4be7ed238127e2bfdc9e9260330d15
Luciano Garriga <lucianomgarriga@gmail.com>
2022-11-14 18:36:54 -0300

    Adding new functions & enums

--- contracts/Manager.sol
+++ contracts/Manager.sol
@@ -1,7 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract Manager {
-  constructor() {
-  }
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Ticket.sol";
+
+contract Manager is Ownable, Ticket {
+    //ver si crear un mapping(address=>[]) u otra opcion
+
+    //receive (){} y fallback(){} ya declarados en Ticket
+
+    constructor(
+        uint256 _id,
+        string memory _eventName,
+        string memory _eventDate,
+        string memory _eventDescription,
+        uint256 _price
+    ) payable Ticket(_id, _eventName, _eventDate, _eventDescription, _price) {}
+
+    function createTicket() public {}
+
+    function showAllTickets() public {}
+
+    function showTicketsByAddress() public {}
+
+    function transferTicket() public onlyOwner {}
+
+    function changeTicketPrice() public onlyOwner {}
+
+    function showStatistitcs() public {}
 }
--- contracts/Ticket.sol
+++ contracts/Ticket.sol
@@ -2,6 +2,62 @@
 pragma solidity ^0.8.17;
 
 contract Ticket {
-  constructor() {
-  }
+    uint256 private id;
+    string private eventName;
+    string private eventDate;
+    string private eventDescription;
+    uint256 private price;
+
+    enum eventType {
+        Sports,
+        Music,
+        Cinema
+    }
+
+    enum TicketStatus {
+        Valid,
+        Used,
+        Expired
+    }
+
+    enum TransferStatus {
+        Transferible,
+        No_Transferible
+    }
+
+    event FundsReceived(uint256 amount);
+
+    receive() external payable {
+        emit FundsReceived(msg.value);
+    }
+
+    fallback() external payable {
+        emit FundsReceived(msg.value);
+    }
+
+    constructor(
+        uint256 _id,
+        string memory _eventName,
+        string memory _eventDate,
+        string memory _eventDescription,
+        uint256 _price
+    ) {
+        id = _id;
+        eventName = _eventName;
+        eventDate = _eventDate;
+        eventDescription = _eventDescription;
+        price = _price;
+    }
+
+    function changePrice() public {}
+
+    function changeTransferStatus() public {}
+
+    function changeStatus() public {}
+
+    function changeOwner() public {}
+
+    function generateId() public {}
+
+    function showInformation() public {}
 }
