1f09022b2bb54dcd68a8ebb7a0425933d4a00312
Luciano Garriga <lucianomgarriga@gmail.com>
2022-12-05 20:16:36 -0300

    Mod migrations & tests

--- migrations/1669590100_migration1.js
+++ migrations/1669590100_migration1.js
@@ -1,10 +1,13 @@
-//MIGRATION para hacer el deploy de un contrato
-//Se agrega una VAR que represente el contrato
-//luego declaramos el contrato que queremos hacer el deploy
+//MIGRATION to deploy a contract
+//Tell TRUFFLE which contract i'd like to interact with
+
 var ManagerContract = artifacts.require("Manager");
+
+//Adding variable which represent a contract
+//Then, we'll declarate the contract that we want to deploy
 
 module.exports = function(deployer) {
   //si el constructor del contrato "Squad" recibe parametros,
   //tambien hay que pasarlo en esta seccion (name)
-  deployer.deploy(ManagerContract);
+  deployer.deploy(ManagerContract, "NFTickets");
 };
--- test/manager.js
+++ test/manager.js
@@ -1,6 +1,6 @@
 const Manager = artifacts.require("Manager");
 const utils = require("./helpers/utils");
- 
+
 contract("Manager", function (accounts) {
 
   let contract;
@@ -10,7 +10,7 @@ contract("Manager", function (accounts) {
   beforeEach(async () => {
     //Must use .new() because we need a new instance for each test 
     //and utilice 1 unique address for each test
-    contract = await Manager.new();
+    contract = await Manager.new("NFTickets");
 
   });
 
@@ -22,7 +22,7 @@ contract("Manager", function (accounts) {
       //In BeforeEach()
 
       //2) ACT = Execute what will be tested
-       await contract.createTicket(
+      await contract.createTicket(
         "Partido Argentina Mexico",
         "26 de Noviembre",
         "Partido Fase de Grupos Qatar",
@@ -31,98 +31,203 @@ contract("Manager", function (accounts) {
         0,
         1100,
         peter
-      ) 
+      )
       //check how many elements the list has and store it in a VAR
       let ticketList = await contract.getTickets();
       assert.equal(ticketList.length, 1, "Ticket list should be 1");
     })
 
-    //2 Test to add a Ticket to list
+    //2 Test to verify it call the event
+    it("should emit event TicketCreated", async function () {
+      // Set up  
+      let ownerOfPlayer = peter;
+      //Act
+      let tx = await contract.createTicket(
+        "Partido Argentina Mexico",
+        "26 de Noviembre",
+        "Partido Fase de Grupos Qatar",
+        0,
+        0,
+        0,
+        1100,
+        ownerOfPlayer
+      )
+
+      let log = tx.logs[0];
+
+      //Assert
+      assert.equal(log.event, "TicketCreated");
+    });
+  });
+
+  context("Function: transferTicket", async function () {
+
+    //Test to verify it transfer the ticket
+    it("should transfer the ticket", async function () {
+      await contract.createTicket(
+        "Partido Argentina Mexico",
+        "26 de Noviembre",
+        "Partido Fase de Grupos Qatar",
+        0,
+        0,
+        0,
+        1100,
+        peter
+      )
+
+      await contract.transferTicket(0, daniel)
+
+      //Assert
+      assert(true);
+    });
+
+    //Test to verify if the ticket is transferible
+    it("should fail if the ticket is not transferible", async function () {
+      await contract.createTicket(
+        "Partido Argentina Mexico",
+        "26 de Noviembre",
+        "Partido Fase de Grupos Qatar",
+        0,
+        0,
+        1,
+        1100,
+        peter
+      )
+
+      await utils.shouldThrow(
+        contract.transferTicket(0, daniel)
+      )
+      //Assert
+      assert(true);
+    });
+
+    //Test to verify if the ticket is valid
+    it("should fail if the ticket is not valid", async function () {
+      await contract.createTicket(
+        "Partido Argentina Mexico",
+        "26 de Noviembre",
+        "Partido Fase de Grupos Qatar",
+        0,
+        1,
+        0,
+        1100,
+        peter
+      )
+
+      await utils.shouldThrow(
+        contract.transferTicket(0, daniel)
+      )
+      //Assert
+      assert(true);
+    });
+
+    //Test to verify it call the event
+    it("should emit event TicketTransfered", async function () {
+      await contract.createTicket(
+        "Partido Argentina Mexico",
+        "26 de Noviembre",
+        "Partido Fase de Grupos Qatar",
+        0,
+        0,
+        0,
+        1100,
+        peter
+      )
+
+      let tx = await contract.transferTicket(0, daniel)
+      let log = tx.logs[0];
+
+      //Assert
+      assert.equal(log.event, "TicketTransfered");
+    });
+
+  })
+
+  context("Function: showStatistitcs", async function () {
+
+    //Test to verify it call the event
+    it("should emit event ViewStatistics", async function () {
+      await contract.createTicket(
+        "Partido Argentina Mexico",
+        "26 de Noviembre",
+        "Partido Fase de Grupos Qatar",
+        0,
+        0,
+        0,
+        1100,
+        peter
+      )
+
+      let tx = await contract.showStatistics()
+      let log = tx.logs[0];
+
+      //Assert
+      assert.equal(log.event, "ViewStatistics");
+    });
+
+  })
+
+  context("Function: deleteTicket", async function () {
+    //1 Test to check the function delete the ticket
+    it("should delete the ticket", async function () {
+      await contract.createTicket(
+        "Partido Argentina Mexico",
+        "26 de Noviembre",
+        "Partido Fase de Grupos Qatar",
+        0,
+        0,
+        0,
+        1100,
+        peter
+      )
+
+      await contract.deleteTicket(0)
+      //check how many elements the list has and store it in a VAR
+      let ticketList = await contract.getTickets();
+      assert.equal(ticketList.length, 0, "Ticket didn't delete");
+    })
+
+    //2 Test to verify if the caller is the owner
     it("should fail if the caller is not the owner", async function () {
       //1) SET UP = Initialize variables 
       let notOwner = daniel;
       //In BeforeEach()
 
       //2) ACT = Execute what will be tested
-      //we take the instance (contract) and call the function (createTicket)
       await utils.shouldThrow(
-      contract.createTicket(
-        "Partido Argentina Polonia",
-        "30 de Noviembre",
-        "Partido Fase de Grupos Qatar",
-        0,
-        0,
-        0,
-        2000,
-        //address who call the function
-        {from: notOwner}
+        contract.createTicket(
+          "Partido Argentina Polonia",
+          "30 de Noviembre",
+          "Partido Fase de Grupos Qatar",
+          0,
+          0,
+          0,
+          2000,
+          { from: notOwner }
         )
       )
     })
 
     //3 Test to verify it call the event
-    // it("should emit event TicketCreated", async function () {
-    //   // Set up  
-    //   let ownerOfPlayer = peter;
-    //   //Act
-    //   let tx = await contract.createTicket(
-    //     "Partido Argentina Mexico",
-    //     "26 de Noviembre",
-    //     "Partido Fase de Grupos Qatar",
-    //     0,
-    //     0,
-    //     0,
-    //     1100,
-    //     ownerOfPlayer
-    //   )
+    it("should emit event TicketDeleted", async function () {
 
-    //   let log = tx.logs[0];
+      await contract.createTicket(
+        "Partido Argentina Mexico",
+        "26 de Noviembre",
+        "Partido Fase de Grupos Qatar",
+        0,
+        0,
+        0,
+        1100,
+        peter
+      )
 
-    //   //Assert
-    //   assert.equal(log.event, "TicketCreated");
-    // });
-  });
-  
-  // context("Function: deleteTicket", async function () {
-  //     //1 Test to check the function delete the ticket
-  //     it("should delete the ticket", async function (){
-  //       await contract.createTicket(
-  //         "Partido Argentina Mexico",
-  //         "26 de Noviembre",
-  //         "Partido Fase de Grupos Qatar",
-  //         0,
-  //         0,
-  //         0,
-  //         1100,
-  //         peter
-  //       )  
+      let tx = await contract.deleteTicket(0)
+      let log = tx.logs[0];
 
-  //       await contract.deleteTicket(0) 
-  //       //check how many elements the list has and store it in a VAR
-  //       let ticketList = await contract.getTickets();
-  //       assert.equal(ticketList.length, 0, "Ticket didn't delete");
-  //     })
+      //Assert
+      assert.equal(log.event, "TicketDeleted");
+    });
+  })
 
-  //     //2 Test to verify it call the event
-  //   it("should emit event TicketDeleted", async function () {
-
-  //     await contract.createTicket(
-  //       "Partido Argentina Mexico",
-  //       "26 de Noviembre",
-  //       "Partido Fase de Grupos Qatar",
-  //       0,
-  //       0,
-  //       0,
-  //       1100,
-  //       peter
-  //     )   
-
-  //     let tx = await contract.deleteTicket(0) 
-  //     let log = tx.logs[0];
-
-  //     //Assert
-  //     assert.equal(log.event, "TicketDeleted");
-  //   });
-    
-  // })
 });
